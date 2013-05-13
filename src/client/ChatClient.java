ff8a6a66d39a89a47ca6b0cea94643996138f6bf
cmobrien <cmobrien@mit.edu>
2013-05-13 18:48:06 -0400

    Updated ChatClient

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,7 +19,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 import server.ChatServer;
 import user.User;
@@ -28,16 +31,16 @@ public class ChatClient extends JFrame {
     private User user;
     private final Map<String, JLabel> userLabels;
     private final JPanel users;
-    private final JPanel onlineUsers;
+    //private final JPanel onlineUsers;
     private final JScrollPane userScroll;
     private final JLabel welcome;
     private final JPanel welcomePanel;
     private final JButton logoutButton;
+    private final JPanel userPanel;
 
     private final ChatClientModel model;
 
     public ChatClient() {
-
         this.model = new ChatClientModel(this);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.addWindowListener(new java.awt.event.WindowAdapter() {
@@ -48,21 +51,37 @@ public class ChatClient extends JFrame {
             }
         });
         
+        userPanel = new JPanel();
+        userPanel.setLayout(new BoxLayout(userPanel, BoxLayout.PAGE_AXIS));
+        
         logoutButton = new JButton();
         logoutButton.setActionCommand("logout");
         logoutButton.addActionListener(model);
 
         userLabels = new HashMap<String, JLabel>();
-
-        onlineUsers = new JPanel();
-        onlineUsers.setLayout(new BoxLayout(onlineUsers, BoxLayout.PAGE_AXIS));
-
+        
         users = new JPanel();
         users.setLayout(new BoxLayout(users, BoxLayout.PAGE_AXIS));
-        users.add(new JLabel("Click on a friend to chat!"));
-        users.add(onlineUsers);
-
         userScroll = new JScrollPane(users);
+        
+        
+        JPanel conversations = new JPanel();
+        conversations.setLayout(new BoxLayout(conversations, BoxLayout.PAGE_AXIS));
+        JScrollPane conversationScroll = new JScrollPane(conversations);
+
+        JPanel userNest = new JPanel();
+        userNest.add(userScroll);
+        userNest.setLayout(new BoxLayout(userNest, BoxLayout.PAGE_AXIS));
+        userNest.setOpaque(false);
+        
+        JPanel conversationNest = new JPanel();
+        conversationNest.add(conversationScroll);
+        conversationNest.setLayout(new BoxLayout(conversationNest, BoxLayout.PAGE_AXIS));
+        conversationNest.setOpaque(false);
+        
+        userPanel.add(userNest);
+        userPanel.add(conversationNest);
+        userPanel.setOpaque(true);
 
         welcome = new JLabel("Log in to start using guichat"); // , " + user.getUsername() + "!");
         welcome.setHorizontalAlignment(JLabel.CENTER);
@@ -72,16 +91,36 @@ public class ChatClient extends JFrame {
 
         // Add color
         users.setBackground(Color.white);
-        onlineUsers.setBackground(Color.white);
-        welcomePanel.setBackground(Color.DARK_GRAY);
+        conversations.setBackground(Color.white);
+        userPanel.setBackground(new Color(0, 51, 102));
+        welcomePanel.setBackground(new Color(0, 51, 102));
         welcome.setForeground(Color.white);
 
         // Add padding
-        Border paddingBorder = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-        onlineUsers.setBorder(BorderFactory.createCompoundBorder(
-                onlineUsers.getBorder(), paddingBorder));
-        users.setBorder(BorderFactory.createCompoundBorder(users.getBorder(),
-                paddingBorder));
+        Border paddingBorder = BorderFactory.createEmptyBorder(5, 10, 5, 10);
+        Border emptyBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+        Border lineBorder = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+        
+        TitledBorder userBorder = BorderFactory.createTitledBorder(emptyBorder, "Friends");
+        userBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
+        userBorder.setTitleColor(Color.white);
+        
+        users.setBorder(paddingBorder);
+        userScroll.setBorder(lineBorder);
+        userNest.setBorder(userBorder);
+        
+        
+        TitledBorder conversationBorder = BorderFactory.createTitledBorder(emptyBorder, "Past Group Chats");
+        conversationBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
+        conversationBorder.setTitleColor(Color.white);
+        
+        conversations.setBorder(paddingBorder);
+        conversationScroll.setBorder(lineBorder);
+        conversationNest.setBorder(conversationBorder);
+        
+        Border outerBorder = BorderFactory.createEmptyBorder(0, 10, 10, 10);
+        userPanel.setBorder(outerBorder);
+        
         welcome.setBorder(BorderFactory.createCompoundBorder(
                 welcome.getBorder(), paddingBorder));
 
@@ -101,9 +140,46 @@ public class ChatClient extends JFrame {
         }
         this.user = new User(username);
         System.out.println("GOT USERNAME");
+        
+        /* For GUI Testing, will delete later
+        addUser(new User("Friend A"));
+        addUser(new User("Friend B"));
+        addUser(new User("Friend C"));
+        addUser(new User("Friend D"));
+        addUser(new User("Friend E"));
+        addUser(new User("Friend F"));
+        addUser(new User("Friend G"));
+        addUser(new User("Friend H"));
+        addUser(new User("Friend I"));
+        addUser(new User("Friend J"));
+        addUser(new User("Friend K"));
+        addUser(new User("Friend L"));
+        addUser(new User("Friend M"));
+        addUser(new User("Friend N"));
+        addUser(new User("Friend O"));
+        addUser(new User("Friend P"));
+        addUser(new User("Friend Q"));
+        addUser(new User("Friend R"));
+        addUser(new User("Friend S"));
+        addUser(new User("Friend T"));
+        addUser(new User("Friend U"));
+        addUser(new User("Friend V"));
+        addUser(new User("Friend W"));
+        addUser(new User("Friend X"));
+        addUser(new User("Friend Y"));
+        addUser(new User("Friend Z"));
 
-        this.setSize(200, 400);
-        welcome.setText("Welcome, " + this.user.getUsername() + "!");
+        conversations.add(new JLabel("tester1"));
+        conversations.add(new JLabel("tester2"));
+        conversations.add(new JLabel("tester3"));
+        conversations.add(new JLabel("tester4"));
+        conversations.add(new JLabel("tester5"));
+        conversations.add(new JLabel("tester6"));
+        conversations.add(new JLabel("tester7"));
+        conversations.add(new JLabel("tester8"));*/
+        
+        this.setSize(200, 500);
+        welcome.setText("<html><font size=+1>Welcome, " + this.user.getUsername() + "!</font></html>");
         this.setVisible(true);
     }
 
@@ -111,13 +187,13 @@ public class ChatClient extends JFrame {
         JLabel userLabel = new JLabel(user.getUsername());
         userLabels.put(user.getUsername(), userLabel);
         new UserListener(userLabel, model, user);
-        onlineUsers.add(userLabel);
+        users.add(userLabel);
         validate();
     }
 
     public void removeUser(String username) {
-        onlineUsers.remove(userLabels.get(username));
-        onlineUsers.revalidate();
+        users.remove(userLabels.get(username));
+        users.revalidate();
     }
 
     private void createGroupLayout() {
@@ -127,12 +203,12 @@ public class ChatClient extends JFrame {
         Group h = layout.createParallelGroup();
         h.addComponent(welcomePanel, GroupLayout.DEFAULT_SIZE,
                 GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
-        h.addComponent(userScroll, GroupLayout.DEFAULT_SIZE,
+        h.addComponent(userPanel, GroupLayout.DEFAULT_SIZE,
                 GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
 
         Group v = layout.createSequentialGroup();
         v.addComponent(welcomePanel, 40, 40, 40);
-        v.addComponent(userScroll, GroupLayout.DEFAULT_SIZE,
+        v.addComponent(userPanel, GroupLayout.DEFAULT_SIZE,
                 GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
 
         layout.setHorizontalGroup(h);
