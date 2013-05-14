0a665db6858da0e473ec5adba1efe2349d585aa7
kathrynsiegel <ksiegel@mit.edu>
2013-05-14 01:59:16 -0400

    fixed testing bug

--- src/client/testing/GroupChatSelectBoxTest.java
+++ src/client/testing/GroupChatSelectBoxTest.java
@@ -5,4 +5,7 @@ public class GroupChatSelectBoxTest {
  * Manually test the group chat select feature by clicking "group chat" on the main
  * ChatClient GUI.
  */
+	public void basicTest() {
+		
+	}
 }
