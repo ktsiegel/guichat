9ca86fc3f463de3d3d673d2d6ad9c679f958fb74
kathrynsiegel <ksiegel@mit.edu>
2013-05-14 00:08:25 -0400

    updated chat history display

--- src/client/HistoryBox.java
+++ src/client/HistoryBox.java
@@ -1,21 +1,55 @@
 package client;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.GroupLayout.Group;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 
 import conversation.ChatHistory;
 
 public class HistoryBox extends JFrame {
+	private JPanel background;
+	private JScrollPane displayScroll;
+	
 	public HistoryBox(ChatHistory history) {
 		this.setSize(300, 300);
-		JPanel background = new JPanel();
 		JTextArea display = new JTextArea();
+		display.setEditable(false);
 		display.setLineWrap(true);
+		displayScroll = new JScrollPane(display);
+		Border lineBorder = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+		Border paddingBorder = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+		display.setBorder(lineBorder);
+        displayScroll.setBorder(lineBorder);
 		display.append(history.getHistory());
-	    JScrollPane displayScroll = new JScrollPane(display);
-	    background.add(display);
-	    this.add(background);
+		background = new JPanel();
+		background.setBackground(new Color(0, 51, 102));
+		background.setBorder(paddingBorder);
+		this.add(background);
+		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+	    createGroupLayout();
 	}
+	
+	private void createGroupLayout() {
+        GroupLayout layout = new GroupLayout(background);
+        background.setLayout(layout);
+
+        Group h = layout.createParallelGroup();
+        h.addComponent(displayScroll, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+
+
+        Group v = layout.createSequentialGroup();
+        v.addComponent(displayScroll, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+
+        layout.setHorizontalGroup(h);
+        layout.setVerticalGroup(v);
+    }
 }
