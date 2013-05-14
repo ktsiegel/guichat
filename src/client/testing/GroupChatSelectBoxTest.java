8b9fd65cef88c60976dd6c72c25d1f42730875b1
kathrynsiegel <ksiegel@mit.edu>
2013-05-14 02:06:13 -0400

    fixed testing error

--- src/client/testing/GroupChatSelectBoxTest.java
+++ src/client/testing/GroupChatSelectBoxTest.java
@@ -1,10 +1,12 @@
 package client.testing;
+import org.junit.Test; 
 
 public class GroupChatSelectBoxTest {
 /**
  * Manually test the group chat select feature by clicking "group chat" on the main
  * ChatClient GUI.
  */
+	@Test
 	public void basicTest() {
 		
 	}
