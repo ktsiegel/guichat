4240d57d535152b89b58c4e2448ab029b94c5d59
kathrynsiegel <ksiegel@mit.edu>
2013-05-16 21:56:13 -0400

    added historylistenertest

---
+++ src/client/testing/ClientListeningThreadTest.java
@@ -0,0 +1,10 @@
+package client.testing;
+
+/**
+ * 
+ * 
+ * @category no_didit
+ */
+
+public class ClientListeningThreadTest {
+}
---
+++ src/client/testing/HistoryListenerTest.java
@@ -0,0 +1,17 @@
+package client.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Testing strategy: 
+ * mouseEntered: Make sure that mousing over a history label highlights it by changing the text to light blue.
+ * mouseExited: Make sure that no longer mousing over a history label changes the text back to black.
+ * mouseClicked: Make sure that clicking a history label opens a HistoryBox containing the history
+ * 				associated with this label.
+ */
+
+public class HistoryListenerTest {
+	public void test() {} //make sure passes didit
+}
